6cc1e2bf1d0274ab9b070798fee94d1b8ba47f06
Ken <kenoh17@uw.edu>
2022-11-07 12:45:07 -0800

    j

--- frumble.sql
+++ frumble.sql
@@ -7,8 +7,29 @@ CREATE TABLE Sales (
 
 
 -- month -> discount
+-- discount -> month
 -- name -> price
+-- month, name -> discount, price
+-- name, discount -> price, month
 
+
+-- no rows where they are the same product but have different prices,
+-- proves name -> price
 SELECT *
-FROM Sales
-ORDER BY name, discount;
+FROM Sales S, Sales S2
+WHERE S.name = S2.name AND
+      S.price != S2.price;
+
+-- no rows where the product is in the same month but have different discounts,
+-- proves month -> discount
+SELECT * 
+FROM Sales S, Sales S2
+WHERE S.month = S2.month AND 
+      S.discount != S2.discount;
+
+-- lots of rows shows that name and discount does not determine the month
+SELECT * 
+FROM Sales S, Sales S2
+WHERE S.name = S2.name AND
+      S.discount = S2.discount AND
+      S.month != S2.month;
