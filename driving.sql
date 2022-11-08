7a9c8fe3950e594798705a48e45b177b26b1c7df
Ken <kenoh17@uw.edu>
2022-11-07 22:51:36 -0800

    k

--- driving.sql
+++ driving.sql
@@ -13,7 +13,8 @@ CREATE TABLE Car (
 
 CREATE TABLE Truck (
     licensePlate VARCHAR(7) PRIMARY KEY REFERENCES Vehicle,
-    capacity INT
+    capacity INT,
+    operatedBy INT REFERENCES ProfessionDriver
 );
 
 CREATE TABLE InsuranceCo (
@@ -37,16 +38,14 @@ CREATE TABLE Driver (
 );
 
 CREATE TABLE NonProfessionalDriver (
-    ssn INT PRIMARY KEY REFERENCES Driver,
-    drives VARCHAR(7) REFERENCES Car
+    ssn INT PRIMARY KEY REFERENCES Driver
 );
 
 CREATE TABLE ProfessionalDriver (
     ssn INT PRIMARY KEY REFERENCES Driver,
-    operates VARCHAR(7) REFERENCES Truck,
-    medicalHistory VARCHAR(MAX)
+    medicalHistory VARCHAR(100)
 );
 
 -- 2b The insures relationship is a many-to-one relationship meaning it can have many vehicles under the same insurance company so I added an insurance company name column to the Vehicles table. I chose to do it this way because each vehicle will have its own insurance company and since there will never be an instance where an insurance company will be by itself outside of the InsuranceCo table, it would reduce redundancy to add it to the Vehicle table.
 
--- 2c "drives" is a many-to-many relationship, meaning multiple different people can drive the same car and multiple cars can be driven by the same person. "Operates" is a many-to-one relationship so one driver can have many trucks, but no truck can have more than one driver.
+-- 2c "drives" is a many-to-many relationship, meaning different people can drive the same car and multiple cars can be driven by the same person, so I need to use a separate table to reduce redundancy. "Operates" is a many-to-one relationship so one driver can have many trucks, but no truck can have more than one driver, so I don't need another table, but rather I add a column under the "many" table.
--- frumble.sql
+++ frumble.sql
@@ -1,3 +1,4 @@
+-- 4.1
 CREATE TABLE Sales (
     name VARCHAR(20),
     discount VARCHAR(5),
@@ -5,12 +6,16 @@ CREATE TABLE Sales (
     price INT
 );
 
-
--- month -> discount
--- discount -> month
+-- 4.2
+-- PROVEN:
 -- name -> price
--- month, name -> discount, price
+-- discount -> month
+-- month -> discount
+-- name, month -> price, discount
 -- name, discount -> price, month
+
+-- DISPROVEN:
+-- price -> name
 
 
 -- no rows where they are the same product but have different prices,
@@ -34,9 +39,80 @@ FROM Sales S, Sales S2
 WHERE S.discount = S2.discount AND
       S.month != S.month;
 
--- lots of rows where the name is different but price is the same,
--- does not prove price -> name
+-- if month -> discount and name -> price, then month, name -> discount, price
+-- this is proven with this query returning no rows
 SELECT *
 FROM Sales S, Sales S2
-WHERE S.price = S2.price AND
-      S.name != S2.name;
+WHERE S.month = S2.month AND
+      S.name = S2.name AND
+      S.discount != S2.discount AND
+      S.price != S2.price;
+
+-- if discount -> month and name -> price, then discount, name -> price, month
+-- this is proven with this query returning no rows
+SELECT *
+FROM Sales S, Sales S2
+WHERE S.discount = S2.discount AND
+      S.name = S2.name AND
+      S.month != S2.month AND
+      S.price != S2.price;
+
+-- more than one name with the price 99
+-- disproves price -> name
+-- 4 rows
+SELECT DISTINCT name
+FROM Sales
+WHERE price = 99;
+
+-- 4.3
+
+-- to solve name -> price, we split Sales(name, price, month, discount) into
+-- S1(name, price) and S2(name, month, discount). S2 can be further decomposed.
+-- Since discount -> month, we can split the table on month, creating S3(name, month)
+-- and S4(month, discount). This solves our discount -> month.
+
+-- Our final BCNF is S1(name, price), S3(name, month), and S4(month, discount).
+
+-- S1(name, price)
+CREATE TABLE S1 (
+      name VARCHAR(20) PRIMARY KEY,
+      price INT
+);
+
+-- S3(name, month)
+CREATE TABLE S3(
+      name VARCHAR(20) REFERENCES S1,
+      month VARCHAR(20) REFERENCES S4
+);
+
+-- S4(month, discount)
+CREATE TABLE S4(
+      month VARCHAR(20) PRIMARY KEY,
+      discount VARCHAR(5)
+);
+
+-- 4.4
+
+-- S1(name, price)
+INSERT INTO S1 (name, price)
+SELECT DISTINCT name, price
+FROM Sales;
+
+-- 36 rows
+SELECT COUNT(*) FROM S1;
+
+-- S3(name, month)
+INSERT INTO S3 (name, month)
+SELECT DISTINCT name, month
+FROM Sales;
+
+-- 426 rows
+SELECT COUNT(*) FROM S3;
+
+-- S4(month, discount)
+INSERT INTO S4 (month, discount)
+SELECT DISTINCT month, discount
+FROM Sales;
+
+-- 12 rows
+SELECT COUNT(*) FROM S4;
